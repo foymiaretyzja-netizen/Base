c78775744f7957d94099dacd12662bdb1308c82d
foymiaretyzja-netizen <foymiaretyzja@gmail.com>
2026-04-09 09:03:38 -0500

    Create gamedata.js

---
+++ public/gamedata.js
@@ -0,0 +1,27 @@
+const gamesLibrary = [
+    {
+        title: "Smash Karts",
+        image: "https://images.crazygames.com/smash-karts/cover-1583232506253.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop",
+        url: "https://smashkarts.io/",
+        featured: true
+    },
+    {
+        title: "1v1.LOL",
+        image: "https://images.crazygames.com/1v1-lol/cover-1594911266224.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop",
+        url: "https://1v1.lol/",
+        featured: true
+    },
+    {
+        title: "Retro Bowl",
+        image: "https://images.crazygames.com/games/retro-bowl/cover-1644917088924.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop",
+        url: "https://retrobowl.me/",
+        featured: false
+    },
+    {
+        title: "Monkey Mart",
+        image: "https://images.crazygames.com/monkey-mart/cover-1675266155940.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop",
+        url: "https://monkeymart.org/",
+        featured: false
+    }
+    // Add as many as you want here!
+];
